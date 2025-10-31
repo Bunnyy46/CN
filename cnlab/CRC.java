caa7572d2490a9c32cb839732bf61fc93b52db95
Bunnyy46 <editjunkie3@gmail.com>
2025-10-31 10:04:46 +0530

    CRC.java

--- cnlab/CRC.java
+++ cnlab/CRC.java
@@ -1,64 +1,35 @@
-package cnlab;
-
 import java.util.*;
 
-public class CRC {
-
+public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Data bits: "); 
-
+        System.out.print("Data bits: ");
         String data = sc.next();
-
-        System.out.print("Generator: "); 
-
+        System.out.print("Generator: ");
         String gen = sc.next();
 
-        String codeword = data + computeCRC(data, gen);
-
+        String rem = computeCRC(data, gen);
+        String codeword = data + rem;
         System.out.println("Transmitted Codeword: " + codeword);
 
-        System.out.print("Received codeword: "); 
-
+        System.out.print("Received codeword: ");
         String recv = sc.next();
-
         System.out.println(computeCRC(recv, gen).contains("1") ? "Error detected!" : "No error detected!");
-
     }
 
     static String computeCRC(String data, String gen) {
-
         int n = gen.length();
+        StringBuilder dividend = new StringBuilder(data).append("0".repeat(n - 1));
+        char[] genArr = gen.toCharArray();
 
-        String tmp = data + "0".repeat(n - 1);
-
-        String div = tmp.substring(0, n);
-
-        for (int i = n; i <= tmp.length(); i++, div = div.substring(1)) {
-
-            if (div.charAt(0) == '1' && i < tmp.length()) div += tmp.charAt(i);
-
-            div = xor(div, gen);
-
+        for (int i = 0; i <= dividend.length() - n; i++) {
+            if (dividend.charAt(i) == '1') {
+                for (int j = 0; j < n; j++)
+                    dividend.setCharAt(i + j, dividend.charAt(i + j) == genArr[j] ? '0' : '1');
+            }
         }
-
-        return div.substring(div.length() - n + 1);
-
+        return dividend.substring(dividend.length() - n + 1);
     }
-
-    static String xor(String a, String b) {
-
-        StringBuilder res = new StringBuilder();
-
-        for (int i = 1; i < b.length(); i++)
-
-            res.append(a.charAt(i) == b.charAt(i) ? '0' : '1');
-
-        return res.toString();
-
-    }
-
 }
+
 
