54c18955ca1ff882cf75cf737397bc8a369de9b1
Bunnyy46 <editjunkie3@gmail.com>
2025-10-31 10:44:07 +0530

    Update six.java

--- cnlab/six.java
+++ cnlab/six.java
@@ -1,43 +1,45 @@
-package cnlab;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+import java.util.*;
 
-        System.out.println("Enter no of nodes");
-        int n=sc.nextInt();
-        int[][] cost=new int[n][n];
-        System.out.println("Enter Adjacency matrix");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cost[i][j]=sc.nextInt();
-            }
-        }
-       int [][] dist=new int[n][n];
-       for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dist[i][j]=cost[i][j];
-            }
-        }
-        for(int k=0;k<n;k++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(dist[i][k]+dist[k][j]<dist[i][j]){
-                    dist[i][j]=dist[i][k]+dist[k][j];
+public class DistanceVectorRouting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+        int[][] cost = new int[n][n];
+
+        System.out.println("Enter adjacency matrix (use 999 for infinity):");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cost[i][j] = sc.nextInt();
+
+        // Initialize distance vectors
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                dist[i][j] = cost[i][j];
+
+        boolean updated;
+        do {
+            updated = false;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                            updated = true;
+                        }
+                    }
                 }
             }
+        } while (updated); // Repeat until no change (convergence)
+
+        // Print routing tables
+        System.out.println("\nRouting tables (distance vector at each node):");
+        for (int i = 0; i < n; i++) {
+            System.out.println("From Node " + i + ":");
+            for (int j = 0; j < n; j++)
+                System.out.print(dist[i][j] + " ");
+            System.out.println();
         }
-    }
-     System.out.print("Routing table");
-      for(int i=0;i<n;i++){
-        System.out.println("From node "+i+" :");
-        for(int j=0;j<n;j++){
-            System.out.println(dist[i][j]+" ");
-        }
-        System.out.println();
-      }
     }
 }
-
-
-
