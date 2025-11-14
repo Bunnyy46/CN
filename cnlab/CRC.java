5b4c38acc3eca17fdd8106e8ae39c975bfb7b15e
Bunnyy46 <editjunkie3@gmail.com>
2025-11-14 10:27:26 +0530

    knn.py

--- cnlab/CRC.java
+++ cnlab/CRC.java
@@ -1,35 +1,18 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Data bits: ");
-        String data = sc.next();
-        System.out.print("Generator: ");
-        String gen = sc.next();
-
-        String rem = computeCRC(data, gen);
-        String codeword = data + rem;
-        System.out.println("Transmitted Codeword: " + codeword);
-
-        System.out.print("Received codeword: ");
-        String recv = sc.next();
-        System.out.println(computeCRC(recv, gen).contains("1") ? "Error detected!" : "No error detected!");
-    }
-
-    static String computeCRC(String data, String gen) {
-        int n = gen.length();
-        StringBuilder dividend = new StringBuilder(data).append("0".repeat(n - 1));
-        char[] genArr = gen.toCharArray();
-
-        for (int i = 0; i <= dividend.length() - n; i++) {
-            if (dividend.charAt(i) == '1') {
-                for (int j = 0; j < n; j++)
-                    dividend.setCharAt(i + j, dividend.charAt(i + j) == genArr[j] ? '0' : '1');
-            }
-        }
-        return dividend.substring(dividend.length() - n + 1);
-    }
-}
-
-
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+X=np.array([[1,19,19000],[1,35,20000],[0,26,43000],[0,27,5700]])
+y=np.array([0,0,1,0])
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
+knn=KNeighborsClassifier(n_neighbors=2)
+knn.fit(X_train,y_train)
+y_pred=knn.predict(X_test)
+print("Actual",y_train)
+print("Pred",y_pred)
+plt.scatter(X_train[:,0],X_train[:,2],c=y_train,s=150,label="actual")
+plt.scatter(X_test[:,0],X_test[:,2],c=y_pred,s=200,marker="X",label="predicted")
+plt.title("KNN")
+plt.grid(True)
+plt.legend()
