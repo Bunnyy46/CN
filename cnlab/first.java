ec0950c8b3dd7063ed750bb10186ed9f9dceb6d7
Bunnyy46 <editjunkie3@gmail.com>
2025-11-14 10:52:31 +0530

    decisiontree.python

--- cnlab/first.java
+++ cnlab/first.java
@@ -1,36 +1,42 @@
-package cnlab;
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
-import java.util.Scanner;
+# Features (X)
+X = [
+    [6, 148, 72, 35, 0, 33.6, 0.627, 50],
+    [1, 85, 66, 29, 0, 26.6, 0.351, 31],
+    [8, 183, 64, 0, 0, 23.3, 0.672, 32],
+    [1, 89, 66, 23, 94, 28.1, 0.167, 21],
+    [0, 137, 40, 35, 168, 43.1, 2.288, 33]
+]
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-        System.out.print("Enter message:");
-        String msg=sc.nextLine();
-        System.err.println("The charcter count is:"+msg.length()+msg);
-
-        System.out.println("Enter your message:");
-        String stuffing=sc.nextLine();
-        String flag ="F",esc="E";
-        String d="F"+stuffing.replace(esc,esc+esc).replace(flag,esc+flag)+"F";
-        System.out.println("The stuffed bit id "+d);
-
-        System.out.println("Enter the bits to stuff");
-        String bit=sc.nextLine();
-        StringBuilder st=new StringBuilder();
-        int count=0;
-        for(int i=0;i<bit.length();i++){
-            st.append(bit.charAt(i));
-            count=(bit.charAt(i)=='1')?count+1:0;
-            if(count==5){
-                st.append('0');
-                count=0;
-            }
-        }
-        System.out.println("The stuffedbit is:"+st);
-    }
-}
+# Target variable (y)
+y = [1, 0, 1, 0, 1]
 
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+model_gini = DecisionTreeClassifier(criterion='gini', max_depth=3)
+model_gini.fit(X_train, y_train)
+y_pred_gini = model_gini.predict(X_test)
+print("Accuracy (Gini):", accuracy_score(y_test, y_pred_gini))
+model_entropy = DecisionTreeClassifier(criterion='entropy', max_depth=3)
+model_entropy.fit(X_train, y_train)
+y_pred_entropy = model_entropy.predict(X_test)
+print("Accuracy (Entropy):", accuracy_score(y_test, y_pred_entropy))
+
+print("Info gain per feature:")
+print(model_entropy.feature_importances_)
+
+plt.subplot(1, 2, 1)
+plot_tree(model_gini)
+plt.title("Decision Tree (Gini)")
+
+plt.subplot(1, 2, 2)
+plot_tree(model_entropy)
+plt.title("Decision Tree (Entropy)")
+
+plt.tight_layout()
+plt.show()
