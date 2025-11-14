de37adc4fc5724c8f8ee26c1a2ed71c2d268e3c6
Bunnyy46 <editjunkie3@gmail.com>
2025-11-14 13:17:04 +0530

    bayesian.java

--- cnlab/three.java
+++ cnlab/three.java
@@ -1,30 +1,37 @@
-package cnlab;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter total number of frames:");
-        int total=sc.nextInt();
-        System.out.println("Enter Window size");
-        int N=sc.nextInt();
-       
-        int sent=0;
-        while(sent<total){
-            System.out.println("\nThe frames sent "+sent+" to "+Math.min(sent+N-1,total-1));
-            System.out.println("Entre the lost frame if not -1:");
-            int lost=sc.nextInt();
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
-            if(lost>=sent && lost<sent+N){
-                System.out.println("frame "+lost+" is lost resend....");
-                sent=lost;
-            }else{
-                System.out.println("frame is succefully acknowldged");
-                sent+=N;
-            }
-        }
-        System.out.println("\nAll frames are sucesfully acknoeldged");
-    }
-}
+# Training sentences
+sentences = [
+    "I love this product",
+    "This movie was amazing",
+    "I am very happy today",
+    "The experience was great",
+    "I enjoyed the service",
+    "I hate this product",
+    "This movie was terrible",
+    "I am very unhappy today",
+    "The experience was awful",
+    "I did not enjoy the service"
+]
 
+labels = [
+    "Positive","Positive","Positive","Positive","Positive",
+    "Negative","Negative","Negative","Negative","Negative"
+]
 
+# Convert text to vectors
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(sentences)
 
+# Train Naive Bayes
+model = MultinomialNB()
+model.fit(X, labels)
+
+# Test sentence
+test_sentence = ["I really love this"]   # change anything here
+X_test = vectorizer.transform(test_sentence)
+
+# Prediction
+print("Input :", test_sentence[0])
+print("Class :", model.predict(X_test)[0])
