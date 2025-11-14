55c3079e2f3ba43232e1d43b42f0850673b09c3c
Bunnyy46 <editjunkie3@gmail.com>
2025-11-14 12:00:45 +0530

    Binary.py

--- cnlab/seven.java
+++ cnlab/seven.java
@@ -1,28 +1,37 @@
-package cnlab;
-import java.util.*;
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
 
-public class seven {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+# Data
+X = np.array([
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [4, 5],
+    [5, 6],
+    [6, 7],
+    [7, 8],
+    [8, 9],
+    [9, 10],
+    [10, 11]
+])
+y = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
 
-        System.out.print("Enter text to encrypt: ");
-        String data = sc.nextLine();
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
-        System.out.print("Enter a single character key: ");
-        char key = sc.next().charAt(0);
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-        String encrypted = "", decrypted = "";
+print("Actual:   ", y_test)
+print("Predicted:", y_pred)
 
-        // Encrypt
-        for (int i = 0; i < data.length(); i++)
-            encrypted += (char)(data.charAt(i) ^ key);
-
-        // Decrypt
-        for (int i = 0; i < encrypted.length(); i++)
-            decrypted += (char)(encrypted.charAt(i) ^ key);
-
-        System.out.println("\nOriginal  : " + data);
-        System.out.println("Encrypted : " + encrypted);
-        System.out.println("Decrypted : " + decrypted);
-    }
-}
+plt.scatter(X_train[:, 0], y_train, color='blue', label='Actual')
+plt.scatter(X_test[:, 0], y_pred, color='red', marker='x', s=100, label='Predicted')
+plt.title('Simple Logistic Regression Visualization')
+plt.xlabel('Feature 1')
+plt.ylabel('Class')
+plt.legend()
+plt.grid(True)
+plt.show()
